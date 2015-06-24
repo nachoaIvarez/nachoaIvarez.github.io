bc031139feeea7b3e5c9d743a281a6c8bd1401f5
Ignacio Alvarez <ignacioalvarez92@gmail.com>
2015-06-24 04:48:59 -0300

    Update 2015-06-24T07:48:55.889Z

--- app.js
+++
@@ -1,2 +0,0 @@
-"format register";System.register("js/main",[],function(e){"use strict";"format es6";var t;return{setters:[],execute:function(){t=666,console.log(""+t)}}});
-//# sourceMappingURL=app.js.map
