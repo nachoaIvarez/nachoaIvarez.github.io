1c0d5b67b513b3d62d46b9858ba356283d1e3f0e
Nacho Álvarez <ignacioalvarez92@gmail.com>
2015-09-08 03:27:59 -0300

    Layout changes and refactors.

---
+++ src/js/dom-helpers.js
@@ -0,0 +1,97 @@
+export function removeClass(element, className) {
+  if (element.classList) {
+    element.classList.remove(className);
+  } else {
+    element.className = element.className.replace(new RegExp('(^|\\b)' + className.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
+  }
+}
+
+export function addClass(element, className) {
+  if (element.classList) {
+    element.classList.add(className);
+  } else {
+    element.className += ' ' + className;
+  }
+}
+
+export function hasClass(element, className) {
+  if (element.classList) {
+    element.classList.contains(className);
+  } else {
+    new RegExp('(^| )' + className + '( |$)', 'gi').test(element.className);
+  }
+}
+
+export function toggleClass(element, className) {
+  if (element.classList) {
+    element.classList.toggle(className);
+  } else {
+    var classes = element.className.split(' ');
+    var existingIndex = classes.indexOf(className);
+
+    if (existingIndex >= 0) {
+      classes.splice(existingIndex, 1);
+    } else {
+      classes.push(className);
+    }
+
+    element.className = classes.join(' ');
+  }
+}
+
+export function ready(fn) {
+  if (document.readyState !== 'loading') {
+    fn();
+  } else {
+    document.addEventListener('DOMContentLoaded', fn);
+  }
+}
+
+function htmlEscape(str) {
+  return str.replace(/&/g, '&amp;') // first!
+    .replace(/>/g, '&gt;')
+    .replace(/</g, '&lt;')
+    .replace(/"/g, '&quot;')
+    .replace(/'/g, '&#39;')
+    .replace(/`/g, '&#96;');
+}
+
+export function html(literalSections, ...substs) {
+  // Use raw literal sections: we don’t want
+  // backslashes (\n etc.) to be interpreted
+  let raw = literalSections.raw;
+
+  let result = '';
+
+  substs.forEach((subst, i) => {
+    // Retrieve the literal section preceding
+    // the current substitution
+    let lit = raw[i];
+
+    // In the example, map() returns an array:
+    // If substitution is an array (and not a string),
+    // we turn it into a string
+    if (Array.isArray(subst)) {
+      subst = subst.join('');
+    }
+
+    // If the substitution is preceded by a dollar sign,
+    // we escape special characters in it
+    if (lit.endsWith('$')) {
+      subst = htmlEscape(subst);
+      lit = lit.slice(0, -1);
+    }
+    result += lit;
+    result += subst;
+  });
+  // Take care of last literal section
+  // (Never fails, because an empty template string
+  // produces one literal section, an empty string)
+  result += raw[raw.length - 1]; // (A)
+
+  return result;
+}
+
+export function capitalize(str) {
+    return str.charAt(0).toUpperCase() + this.slice(1);
+}
